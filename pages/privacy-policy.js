b423b23c8cfed9a5a1edc9b66327ff9d37ddb403
Eduardo <eduardo@ominitrix.com>
2024-10-04 11:33:10 -0300

    main

---
+++ pages/privacy-policy.js
@@ -0,0 +1,183 @@
+import Head from 'next/head';
+
+export default function PoliticaDePrivacidade() {
+  return (
+    <>
+      <Head>
+        <title>Política de Privacidade</title>
+      </Head>
+      <div style={{ padding: '1em', fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif" }}>
+        <strong>Política de Privacidade</strong>
+        <p>
+          Esta política de privacidade se aplica ao aplicativo Ominitrix (doravante referido como
+          "Aplicativo") para dispositivos móveis que foi criado por Eduardo (doravante referido como
+          "Prestador de Serviço") como um serviço gratuito. Este serviço é fornecido "COMO ESTÁ".
+        </p>
+        <br />
+        <strong>Coleta e Uso de Informações</strong>
+        <p>
+          O Aplicativo coleta informações quando você o baixa e utiliza. Essas informações podem
+          incluir:
+        </p>
+        <ul>
+          <li>O endereço IP do seu dispositivo (por exemplo, endereço IP)</li>
+          <li>
+            As páginas do Aplicativo que você visita, a hora e a data da sua visita, o tempo gasto
+            nessas páginas
+          </li>
+          <li>O tempo gasto no Aplicativo</li>
+          <li>O sistema operacional que você usa em seu dispositivo móvel</li>
+        </ul>
+        <p>O Aplicativo não coleta informações precisas sobre a localização do seu dispositivo móvel.</p>
+        {/* O seguinte div está definido como display: none; */}
+        <div style={{ display: 'none' }}>
+          <p>
+            O Aplicativo coleta a localização do seu dispositivo, o que ajuda o Prestador de Serviço
+            a determinar sua localização geográfica aproximada e fazer uso dela das seguintes
+            maneiras:
+          </p>
+          <ul>
+            <li>
+              Serviços de Geolocalização: O Prestador de Serviço utiliza dados de localização para
+              fornecer recursos como conteúdo personalizado, recomendações relevantes e serviços
+              baseados em localização.
+            </li>
+            <li>
+              Análises e Melhorias: Dados de localização agregados e anônimos ajudam o Prestador de
+              Serviço a analisar o comportamento do usuário, identificar tendências e melhorar o
+              desempenho e a funcionalidade geral do Aplicativo.
+            </li>
+            <li>
+              Serviços de Terceiros: Periodicamente, o Prestador de Serviço pode transmitir dados de
+              localização anônimos para serviços externos. Esses serviços os auxiliam a aprimorar o
+              Aplicativo e otimizar suas ofertas.
+            </li>
+          </ul>
+        </div>
+        <br />
+        <p>
+          O Prestador de Serviço pode usar as informações que você forneceu para contatá-lo de
+          tempos em tempos para fornecer informações importantes, avisos necessários e promoções de
+          marketing.
+        </p>
+        <br />
+        <p>
+          Para uma melhor experiência ao usar o Aplicativo, o Prestador de Serviço pode exigir que
+          você forneça certas informações pessoalmente identificáveis, incluindo, mas não se
+          limitando a, E-mail. As informações que o Prestador de Serviço solicitar serão retidas por
+          eles e usadas conforme descrito nesta política de privacidade.
+        </p>
+        <br />
+        <strong>Acesso de Terceiros</strong>
+        <p>
+          Apenas dados agregados e anônimos são periodicamente transmitidos para serviços externos
+          para ajudar o Prestador de Serviço a melhorar o Aplicativo e seus serviços. O Prestador de
+          Serviço pode compartilhar suas informações com terceiros nas maneiras descritas nesta
+          declaração de privacidade.
+        </p>
+        <div>
+          <br />
+          <p>
+            Observe que o Aplicativo utiliza serviços de terceiros que possuem suas próprias
+            Políticas de Privacidade sobre o manuseio de dados. Abaixo estão os links para a Política
+            de Privacidade dos provedores de serviços de terceiros usados pelo Aplicativo:
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://www.google.com/policies/privacy/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Serviços do Google Play
+              </a>
+            </li>
+          </ul>
+        </div>
+        <br />
+        <p>
+          O Prestador de Serviço pode divulgar Informações Fornecidas pelo Usuário e Informações
+          Coletadas Automaticamente:
+        </p>
+        <ul>
+          <li>conforme exigido por lei, como para cumprir uma intimação ou processo legal semelhante;</li>
+          <li>
+            quando acreditam de boa fé que a divulgação é necessária para proteger seus direitos,
+            proteger sua segurança ou a segurança de outros, investigar fraude ou responder a uma
+            solicitação governamental;
+          </li>
+          <li>
+            com seus provedores de serviços confiáveis que trabalham em seu nome, não têm um uso
+            independente das informações que divulgamos a eles e concordaram em aderir às regras
+            estabelecidas nesta declaração de privacidade.
+          </li>
+        </ul>
+        <br />
+        <strong>Direitos de Exclusão</strong>
+        <p>
+          Você pode parar toda a coleta de informações pelo Aplicativo facilmente desinstalando-o.
+          Você pode usar os processos padrão de desinstalação que podem estar disponíveis como parte
+          do seu dispositivo móvel ou via marketplace ou rede de aplicativos móveis.
+        </p>
+        <br />
+        <strong>Política de Retenção de Dados</strong>
+        <p>
+          O Prestador de Serviço reterá os dados fornecidos pelo usuário enquanto você utilizar o
+          Aplicativo e por um tempo razoável após isso. Se você deseja que eles excluam os Dados
+          Fornecidos pelo Usuário que você forneceu através do Aplicativo, por favor, entre em
+          contato com eles em contato@ominitrix.com e eles responderão em um tempo razoável.
+        </p>
+        <br />
+        <strong>Crianças</strong>
+        <p>
+          O Prestador de Serviço não usa o Aplicativo para, conscientemente, solicitar dados de ou
+          fazer marketing para crianças menores de 13 anos.
+        </p>
+        <div>
+          <br />
+          <p>
+            O Aplicativo não se destina a ninguém com menos de 13 anos de idade. O Prestador de
+            Serviço não coleta intencionalmente informações pessoalmente identificáveis de crianças
+            menores de 13 anos. No caso de o Prestador de Serviço descobrir que uma criança menor de
+            13 anos forneceu informações pessoais, eles as excluirão imediatamente de seus servidores.
+            Se você é um pai ou responsável e está ciente de que seu filho nos forneceu informações
+            pessoais, por favor, entre em contato com o Prestador de Serviço (contato@ominitrix.com)
+            para que eles possam tomar as medidas necessárias.
+          </p>
+        </div>
+        <br />
+        <strong>Segurança</strong>
+        <p>
+          O Prestador de Serviço está preocupado em salvaguardar a confidencialidade de suas
+          informações. O Prestador de Serviço fornece proteções físicas, eletrônicas e procedimentais
+          para proteger as informações que processam e mantêm.
+        </p>
+        <br />
+        <strong>Alterações</strong>
+        <p>
+          Esta Política de Privacidade pode ser atualizada de tempos em tempos por qualquer motivo. O
+          Prestador de Serviço notificará você sobre quaisquer alterações à Política de Privacidade
+          atualizando esta página com a nova Política de Privacidade. Aconselhamos que você consulte
+          esta Política de Privacidade regularmente para quaisquer alterações, pois o uso continuado
+          é considerado aprovação de todas as alterações.
+        </p>
+        <br />
+        <p>Esta política de privacidade é efetiva a partir de 04/10/2024</p>
+        <br />
+        <strong>Seu Consentimento</strong>
+        <p>
+          Ao usar o Aplicativo, você está consentindo com o processamento de suas informações conforme
+          estabelecido nesta Política de Privacidade agora e conforme alterada por nós.
+        </p>
+        <br />
+        <strong>Contate-nos</strong>
+        <p>
+          Se você tiver quaisquer dúvidas relacionadas à privacidade ao usar o Aplicativo, ou tiver
+          perguntas sobre as práticas, por favor, entre em contato com o Prestador de Serviço via
+          e-mail em contato@ominitrix.com.
+        </p>
+        <hr />
+      </div>
+    </>
+  );
+}
---
+++ pages/terms-conditions.js
@@ -0,0 +1,73 @@
+import Head from 'next/head';
+
+export default function Terms() {
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Termos e Condições</title>
+        <style>{`
+          body {
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            padding: 1em;
+          }
+        `}</style>
+      </Head>
+      <main>
+        <strong>Termos e Condições</strong>
+        <br />
+        <p>Estes termos e condições se aplicam ao aplicativo Ominitrix (doravante referido como "Aplicativo") para dispositivos móveis que foi criado por Eduardo (doravante referido como "Provedor de Serviço") como um serviço gratuito.</p>
+        <br />
+        <p>
+          Ao baixar ou utilizar o Aplicativo, você está automaticamente concordando com os termos a seguir. É fortemente recomendado que você leia e compreenda estes termos antes de usar o Aplicativo. A cópia não autorizada, modificação do Aplicativo, de qualquer parte do Aplicativo ou de nossas marcas comerciais é estritamente proibida. Qualquer tentativa de extrair o código-fonte do Aplicativo, traduzi-lo para outros idiomas ou criar versões derivadas não é permitida. Todas as marcas comerciais, direitos autorais, direitos de banco de dados e outros direitos de propriedade intelectual relacionados ao Aplicativo permanecem de propriedade do Provedor de Serviço.
+        </p>
+        <br />
+        <p>
+          O Provedor de Serviço se dedica a garantir que o Aplicativo seja o mais útil e eficiente possível. Assim, ele se reserva o direito de modificar o Aplicativo ou cobrar por seus serviços a qualquer momento e por qualquer motivo. O Provedor de Serviço garante que quaisquer cobranças pelo Aplicativo ou seus serviços serão claramente comunicadas a você.
+        </p>
+        <br />
+        <p>
+          O Aplicativo armazena e processa os dados pessoais que você forneceu ao Provedor de Serviço para fornecer o Serviço. É sua responsabilidade manter a segurança do seu telefone e o acesso ao Aplicativo. O Provedor de Serviço recomenda fortemente que você não faça jailbreak ou root em seu telefone, pois isso remove as restrições e limitações de software impostas pelo sistema operacional oficial do seu dispositivo. Essas ações podem expor seu telefone a malware, vírus, programas maliciosos, comprometer os recursos de segurança do telefone e resultar no mau funcionamento ou falha total do Aplicativo.
+        </p>
+        <div>
+          <p>Observe que o Aplicativo utiliza serviços de terceiros que têm seus próprios Termos e Condições. Abaixo estão os links para os Termos e Condições dos prestadores de serviços de terceiros utilizados pelo Aplicativo:</p>
+          <ul>
+            <li>
+              <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">Google Play Services</a>
+            </li>
+          </ul>
+        </div>
+        <br />
+        <p>
+          Por favor, esteja ciente de que o Provedor de Serviço não assume responsabilidade por certos aspectos. Algumas funções do Aplicativo requerem uma conexão ativa com a internet, que pode ser fornecida por Wi-Fi ou pela sua operadora de rede móvel. O Provedor de Serviço não pode ser responsabilizado se o Aplicativo não funcionar em plena capacidade devido à falta de acesso ao Wi-Fi ou caso você tenha esgotado sua franquia de dados.
+        </p>
+        <br />
+        <p>
+          Se você estiver utilizando o Aplicativo fora de uma área com Wi-Fi, esteja ciente de que os termos do acordo da sua operadora de rede móvel ainda se aplicam. Consequentemente, sua operadora pode cobrar por dados utilizados enquanto estiver conectado ao Aplicativo ou outros encargos de terceiros. Ao utilizar o Aplicativo, você aceita a responsabilidade por quaisquer encargos, incluindo taxas de roaming de dados, se usar o Aplicativo fora de sua área de origem (isto é, região ou país) sem desativar o roaming de dados. Se você não for o pagador da conta do dispositivo em que está utilizando o Aplicativo, assume-se que você obteve permissão do pagador da conta.
+        </p>
+        <br />
+        <p>
+          Da mesma forma, o Provedor de Serviço não pode sempre assumir responsabilidade pelo uso que você faz do Aplicativo. Por exemplo, é sua responsabilidade garantir que seu dispositivo permaneça carregado. Se o dispositivo ficar sem bateria e você não puder acessar o Serviço, o Provedor de Serviço não poderá ser responsabilizado.
+        </p>
+        <br />
+        <p>
+          No que diz respeito à responsabilidade do Provedor de Serviço pelo uso que você faz do Aplicativo, é importante observar que, embora se esforcem para garantir que o Aplicativo esteja sempre atualizado e correto, eles dependem de terceiros para fornecer informações para disponibilizá-las a você. O Provedor de Serviço não assume responsabilidade por qualquer perda, direta ou indireta, que você possa experimentar como resultado de confiar completamente na funcionalidade do Aplicativo.
+        </p>
+        <br />
+        <p>
+          O Provedor de Serviço pode desejar atualizar o Aplicativo em algum momento. O Aplicativo está atualmente disponível conforme os requisitos para o sistema operacional (e para quaisquer sistemas adicionais que eles decidam estender a disponibilidade do Aplicativo) possam mudar, e você precisará baixar as atualizações se quiser continuar a usar o Aplicativo. O Provedor de Serviço não garante que sempre atualizará o Aplicativo para que seja relevante para você e/ou compatível com a versão do sistema operacional instalada no seu dispositivo. No entanto, você concorda em sempre aceitar atualizações do Aplicativo quando oferecidas a você. O Provedor de Serviço também pode desejar encerrar a disponibilização do Aplicativo e pode rescindir seu uso a qualquer momento sem fornecer aviso de rescisão. A menos que informem o contrário, com a rescisão (a) os direitos e licenças concedidos a você nestes termos cessarão; (b) você deve parar de usar o Aplicativo e, se necessário, excluí-lo do seu dispositivo.
+        </p>
+        <br />
+        <strong>Alterações a Estes Termos e Condições</strong>
+        <p>O Provedor de Serviço pode atualizar periodicamente os Termos e Condições. Portanto, recomenda-se que você revise esta página regularmente para qualquer alteração. O Provedor de Serviço notificará você de quaisquer mudanças postando os novos Termos e Condições nesta página.</p>
+        <br />
+        <p>Estes termos e condições são eficazes a partir de 2024-10-04.</p>
+        <br />
+        <strong>Contato</strong>
+        <p>Se você tiver alguma dúvida ou sugestão sobre os Termos e Condições, não hesite em entrar em contato com o Provedor de Serviço em contato@ominitrix.com.</p>
+        <hr />
+      </main>
+    </>
+  );
+}
